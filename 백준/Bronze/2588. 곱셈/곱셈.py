b6a283ce8e608c964e85f7d41728d330fb1c39aa
whitecirclesnake <128918447+whitecirclesnake@users.noreply.github.com>
2023-04-17 03:20:41 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,9 +1,8 @@
 a = int(input())
 b = int(input())
-c = b // 100 ### 3
-d = (b - (c * 100)) // 10 ###8
-e = b - (c * 100) - (d * 10)
-
+c = (b // 100)
+d = ((b - (c * 100)) // 10)
+e = (b - (c * 100) - (d * 10))
 print(a * e)
 print(a * d)
 print(a * c)
